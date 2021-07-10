c55e43a516e01dbfa16e4217a30e455bf023e954
Ava Pimmel Correa <avapcorrea@gmail.com>
2021-07-10 17:03:53 -0300

    Debbuging mongoDB

--- src/data/database.js
+++ src/data/database.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-const connect = async () => {
-   await mongoose.connect(process.env.DATABASE_URL,{
+const connect = () => {
+    mongoose.connect(process.env.DATABASE_URL,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
